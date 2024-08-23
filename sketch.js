b4e73c92033c152f83303a93caddca6fe99d45a2
giovanegorski <giovane.gorski.silva@escola.pr.gov.br>
2024-08-23 10:51:29 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+    background("rgb(209,209,20)")
+}
 
+function draw() {
+
+  stroke("purple");
+  fill("#F4B907");
+  
+  //console.log(mouseIsPressed)
+  if  (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 30);
+  }
+  
+}
